9471ae99cb5951e8ddec7a2d7ac5c635f76388ad
gh0sty02 <pranayyadav08@gmail.com>
2023-11-14 13:01:11 +0530

    fix: fixed null as lastname from google

--- lib/current-profile.ts
+++ lib/current-profile.ts
@@ -15,5 +15,19 @@ export const getCurrentUser = async () => {
     },
   });
 
+  // if last name is not available, it is set to 'null' as default, so remove it
+  if (profile?.name.split(" ")[1] === "null") {
+    return await db.profile.update({
+      where: {
+        userId,
+      },
+      data: {
+        name: profile?.name
+          .split(" ")
+          .filter((chunk) => chunk !== "null")
+          .join(" "),
+      },
+    });
+  }
   return profile;
 };
